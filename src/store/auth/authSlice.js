a996eb9871732e373d90b98b723360afd0afc1fd
Sand128 <m.sandy.av@gmail.com>
2025-08-04 18:26:00 -0600

    se cambio el valor del status por authenticated para entrar al menu

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'checking', // 'checking', 'authenticated'
+        status: 'authenticated', // 'checking', 'authenticated'
         token: null,
         name: null,
         email: null,
