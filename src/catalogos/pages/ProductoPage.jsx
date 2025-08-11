bf635d56be38b50f0d90b071ec77a8366f734ca8
Sand128 <m.sandy.av@gmail.com>
2025-08-11 14:34:13 -0600

    mejorar diseño de la sección productos con botones centrados, título y botones ahora están alineados horizontalmente con estilos directos"

--- src/catalogos/pages/ProductoPage.jsx
+++ src/catalogos/pages/ProductoPage.jsx
@@ -1,7 +1,16 @@
 export const ProductoPage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Productos</h1>
-    </div>
+    <>
+      <div style={{ display: 'flex', justifyContent: "center", alignItems:"center", marginTop:"50px",gap:"50px"}}>
+        <h1>Productos</h1>
+        <button style={{ backgroundColor: "#9abdffff", color: "white", padding: "10px 20px", border: "none", borderRadius: "5px", cursor: "pointer", fontSize: "16px", transition: "transform 0.6s cubic-bezier(.08, .35, .13, 1.02), opacity 0.4s" }}>
+          Listar
+        </button>
+        <button style={{ backgroundColor: "#9abdffff", color: "white", padding: "10px 20px", border: "none", borderRadius: "5px", cursor: "pointer", fontSize: "16px", transition: "transform 0.6s cubic-bezier(.08, .35, .13, 1.02), opacity 0.4s" }}>
+          Registrar
+        </button>
+      </div>
+    </>    
+    
   )
 }
